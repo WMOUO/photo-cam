d80164903891947f27be928be3f7da8e39cdf1d4
WMOUO <5316eictlws.2@gmail.com>
2025-03-15 15:37:39 +0800

    移除dist(我來了cloudflare

--- vite.config.ts
+++ vite.config.ts
@@ -7,12 +7,11 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(() => ({
   plugins: [vue(), vueJsx(), vueDevTools(), tailwindcss()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-  base: '/photo-cam/',
-})
+}))
